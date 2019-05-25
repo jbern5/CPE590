252362c01bce54ab6b439c061896d9281047900c
Justin Bernstein <justinbernstein5@gmail.com>
2019-05-24 21:39:21 -0400

    added makefile

--- FizzBuzz/fizzbuzz.cpp
+++ FizzBuzz/fizzbuzz.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 int main(){
 	for(int i = 74; i <= 291; i++){
-		if(i % 3 == 0){
+		if(i % 3 == 0 && i % 5 == 0){
+			cout << "BuzzFizz" << endl;
+		}
+		else if(i % 3 == 0){
 			cout << "Buzz" << endl;
 		}
 		else if(i % 5 == 0){
 			cout << "Fizz" << endl;
 		}
-		else if(i % 3 == 0 && i % 5 == 0){
-			cout << "BuzzFizz" << endl;
 		else{
 			cout << i << endl;
 		}
